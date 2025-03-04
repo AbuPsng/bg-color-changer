d8999f78b1ef0a2c3c469f0504def9610b916da3
Phassang <abupsngdscz@gmail.com>
2025-03-04 09:51:08 +0530

    Add README.md file

---
+++ README.md
@@ -0,0 +1,5 @@
+Bg Color change is my personal project to understand the dom manipulation better
+
+And also to enhance my grasp of clouser in more detail
+
+This is not a complete project.. I have yet to implement the closure..although my code is running fine without closure
--- app.js
+++ app.js
@@ -6,54 +6,37 @@ const randomBtnElement = document.getElementById("randomBtn");
 const colorBtnContainerElement = document.getElementById("color-btn-container");
 const sectionElement = document.getElementsByTagName("section")[0];
 
-//***** Variable Declaration *****/
-
-const colorCharacter = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-];
-
 //***** function declartion *****/
 
 //generate random color
-function generateRandomColor() {
-  let randomColor = "#";
-  for (let i = 0; i < 6; i++) {
-    const randomNumber = Math.round(Math.random() * 15);
-    randomColor += colorCharacter[randomNumber];
-  }
-  console.log(randomColor, "");
-  colorPickerElement.style.background = randomColor;
-  colorPickerElement.value = randomColor;
-}
+const generateRandomColor = (function () {
+  const colorCharacter = "0123456789abcdef";
+  return function () {
+    let randomColor = "#";
+    for (let i = 0; i < 6; i++) {
+      const randomNumber = Math.floor(Math.random() * 16);
+      randomColor += colorCharacter[randomNumber];
+    }
+    colorPickerElement.style.background = randomColor;
+    colorPickerElement.value = randomColor;
+  };
+})();
 
 //create buttons
-function createColorBtn(hexColor) {
-  const colorBtn = document.createElement("button");
-  console.log(typeof hexColor);
-  colorBtn.style.backgroundColor = hexColor;
-  colorBtn.innerText = hexColor;
-  colorBtn.classList.add("button");
-  colorBtn.addEventListener("click", () => {
-    console.log("first");
-    sectionElement.style.backgroundColor = hexColor;
-  });
-  colorBtnContainerElement.appendChild(colorBtn);
-}
+const createColorButton = (function () {
+  return function (hexColor) {
+    const colorBtn = document.createElement("button");
+    colorBtn.style.backgroundColor = hexColor;
+    colorBtn.innerText = hexColor;
+    colorBtn.classList.add("button");
+
+    colorBtn.addEventListener("click", () => {
+      sectionElement.style.backgroundColor = hexColor;
+    });
+
+    colorBtnContainerElement.appendChild(colorBtn);
+  };
+})();
 
 //take input and create btn
 function submitHandler(e) {
@@ -63,16 +46,27 @@ function submitHandler(e) {
     alert("Please provide some hex color");
     return;
   }
-  createColorBtn(colorPickerElement.value);
+  createColorButton(colorPickerElement.value);
   colorPickerElement.value = "";
 }
 
+function addClickEventOnce(element, cb) {
+  let isEventAdded = false;
+  return function () {
+    if (!isEventAdded) {
+      element.addEventListener("click", cb);
+      isEventAdded = true;
+    }
+  };
+}
 //***** assigning funtions to respective element *****/
 
-createBtnElement.addEventListener("click", (e) => {
-  submitHandler(e);
-});
+const addOneClickCreateBtn = addClickEventOnce(createBtnElement, submitHandler);
 
-randomBtnElement.addEventListener("click", () => {
-  generateRandomColor();
-});
+const addOneClickRandomBtn = addClickEventOnce(
+  randomBtnElement,
+  generateRandomColor
+);
+
+addOneClickCreateBtn();
+addOneClickRandomBtn();
